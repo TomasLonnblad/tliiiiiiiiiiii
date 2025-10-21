b1420b406a4c769ad0fed4c68df0c854193babc6
Tomas <tomaslonnblad@gmail.com>
2025-10-21 06:36:25 +0200

    Increase spotlight angle from 0.12 to 5.12

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -14,7 +14,7 @@ const Computers = ({ isMobile }) => {
       <hemisphereLight intensity={0.15} groundColor='black' />
       <spotLight
         position={[-20, 60, 10]}
-        angle={0.12}
+        angle={5.12}
         penumbra={1}
         intensity={1}
         castShadow
