022b31612f45111f520f43bf8577a437673ea95f
Tomas <tomaslonnblad@gmail.com>
2025-10-21 07:30:07 +0200

    Increase camera FOV from 75 to 90

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -60,7 +60,7 @@ const ComputersCanvas = () => {
       frameloop='demand'
       shadows
       dpr={[1, 2]}
-      camera={{ position: [0.1, 0.1, 0.1], fov: 75 }}
+      camera={{ position: [0.1, 0.1, 0.1], fov: 90 }}
       gl={{ preserveDrawingBuffer: true }}
     >
       <Suspense fallback={<CanvasLoader />}>
