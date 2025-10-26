f61f86730f79b5e477b575afdec7aebdf145170f
Tomas <tomaslonnblad@gmail.com>
2025-10-26 08:19:12 +0100

    Update Computers.jsx

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,4 +1,4 @@
-import  { gltf } from "gltf"
+import  { gltf } from "gltf";
 import React, { Suspense, useEffect, useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
