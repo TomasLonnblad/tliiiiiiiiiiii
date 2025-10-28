8df6339f01aa15e837c399534416cbe0dd2218b4
Tomas <tomaslonnblad@gmail.com>
2025-10-28 09:17:44 +0100

    Update Earth.jsx

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -1,7 +1,7 @@
 import React, { Suspense } from "react";
-import { Canvas } from "@react-three/fiber";
+//import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
-import CanvasLoader from "../Loader";
+//import CanvasLoader from "../Loader";
 
 const Earth = () => {
   const earth = useGLTF("/planet/scene.gltf");
@@ -11,7 +11,7 @@ const Earth = () => {
   );
 };
 
-const EarthCanvas = () => {
+//const EarthCanvas = () => {
   return (
     <Canvas
       shadows
@@ -40,4 +40,4 @@ const EarthCanvas = () => {
   );
 };
 
-export default EarthCanvas;
+//export default EarthCanvas;
